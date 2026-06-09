9484c055f94f2f4b723181c25a540978841d06bd
Leo Dulanto <leotens11@gmail.com>
2026-06-09 13:47:27 -0500

    feat: Add start file for lesson 4

---
+++ starter-files/lesson4/lesson4.java
@@ -0,0 +1,28 @@
+//Scanner
+
+//Clases
+//class{}
+
+public class Main
+{
+
+    //Métodos
+
+    //1. Sin parámetros
+
+    //2. Con parámetros
+
+
+    public static void main(String[] args) {
+
+        //LLamado de metodos
+
+        //Creación Scanner
+
+
+        //Uso de Clases
+
+
+        //Cerrar Scanner
+    }
+}
