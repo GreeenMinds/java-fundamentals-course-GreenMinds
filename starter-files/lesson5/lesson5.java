d982ad64565d5039557329f9c0c8a006647be7d4
Isabel <u20241e158@upc.edu.pe>
2026-06-11 21:24:22 -0500

    feat: Add start file for lesson 5

---
+++ starter-files/lesson5/lesson5.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) {
+        // Crear Scanner y Registro
+        // Menu con do-while: agregar, listar, promedio, salir
+    }
+}
+
+// Maneja todos los estudiantes
+class Registro {
+    // Arreglo de Estudiante y cantidad
+    // Metodos: agregar, listar, mostrarPromedio
+}
+
+// Representa a un estudiante
+class Estudiante {
+    // Atributos privados: nombre, edad, nota
+    // Getters, Setters (nota con validacion)
+    // Metodo aprobo
+}
